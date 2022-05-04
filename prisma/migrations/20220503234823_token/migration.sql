2a765e7fa5643f2634d279f8aba02b59a772c0d9
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-05-04 19:55:27 -0300

    chore: migrate prisma schema

---
+++ prisma/migrations/20220503234823_token/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "Token" (
+    "id" SERIAL NOT NULL,
+    "access_token" TEXT NOT NULL,
+    "refresh_token" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Token_access_token_key" ON "Token"("access_token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Token_refresh_token_key" ON "Token"("refresh_token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Token_userId_key" ON "Token"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Token" ADD CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
