f82de9106717c369b7422e8b462a0e25f2e12ca8
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-05-04 19:59:06 -0300

    refactor: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { JwtGuard } from './modules/auth/guards/jwt.guard';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { TokenModule } from './modules/token/token.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, HttpModule, AuthModule, TokenModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [{ provide: APP_GUARD, useClass: JwtGuard }, AppService],
 })
 export class AppModule {}
