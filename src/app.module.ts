00a2eb81ef1be32b589238b8df240ac0846c3f6c
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-05-16 19:05:15 -0300

    refactor: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { JwtGuard } from './modules/auth/guards/jwt.guard';
+import { BoardsModule } from './modules/boards/boards.module';
+import { NotesModule } from './modules/notes/notes.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { TokenModule } from './modules/token/token.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PrismaModule, HttpModule, AuthModule, TokenModule, UsersModule],
+  imports: [
+    PrismaModule,
+    HttpModule,
+    AuthModule,
+    TokenModule,
+    UsersModule,
+    BoardsModule,
+    NotesModule,
+  ],
   controllers: [AppController],
   providers: [{ provide: APP_GUARD, useClass: JwtGuard }, AppService],
 })
