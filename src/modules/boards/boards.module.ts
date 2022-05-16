4bed6e6f04ee2b9ab96f99591faa778d9210c68b
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-05-16 19:03:38 -0300

    refactor: update boards module

--- src/modules/boards/boards.module.ts
+++ src/modules/boards/boards.module.ts
@@ -2,11 +2,37 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { CreateBoardController } from './controllers/create.controller';
+import { FindAllBoardsController } from './controllers/findAll.controller';
+import { FindOneBoardController } from './controllers/findOne.controller';
+import { UpdateBoardController } from './controllers/update.controller';
+import { CreateBoardDb } from './database/create.db';
+import { FindAllBoardsDb } from './database/findAll.db';
+import { FindOneBoardDb } from './database/findOne.db';
+import { UpdateBoardDb } from './database/update.db';
+import { CreateBoardService } from './services/create.service';
+import { FindAllBoardsService } from './services/findAll.service';
+import { FindOneBoardService } from './services/findOne.service';
+import { UpdateBoardService } from './services/update.service';
 
 @Module({
   imports: [HttpModule, PrismaModule, AuthModule],
-  controllers: [],
-  providers: [],
+  controllers: [
+    CreateBoardController,
+    UpdateBoardController,
+    FindAllBoardsController,
+    FindOneBoardController,
+  ],
+  providers: [
+    CreateBoardService,
+    UpdateBoardService,
+    FindAllBoardsService,
+    FindOneBoardService,
+    CreateBoardDb,
+    UpdateBoardDb,
+    FindAllBoardsDb,
+    FindOneBoardDb,
+  ],
   exports: [],
 })
 export class BoardsModule {}
