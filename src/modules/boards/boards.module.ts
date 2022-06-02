d636a90fa0da04b886d6ae831e27f3dd454791a2
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-06-02 19:27:05 -0300

    refactor: update boards module

--- src/modules/boards/boards.module.ts
+++ src/modules/boards/boards.module.ts
@@ -1,8 +1,6 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
-import { DeleteNoteDb } from '../notes/database/delete.db';
-import { DeleteNoteService } from '../notes/services/delete.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UsersModule } from '../users/users.module';
 import { CreateBoardController } from './controllers/create.controller';
@@ -11,10 +9,12 @@ import { FindAllBoardsController } from './controllers/findAll.controller';
 import { FindOneBoardController } from './controllers/findOne.controller';
 import { UpdateBoardController } from './controllers/update.controller';
 import { CreateBoardDb } from './database/create.db';
+import { DeleteBoardDb } from './database/delete.db';
 import { FindAllBoardsDb } from './database/findAll.db';
 import { FindOneBoardDb } from './database/findOne.db';
 import { UpdateBoardDb } from './database/update.db';
 import { CreateBoardService } from './services/create.service';
+import { DeleteBoardService } from './services/delete.service';
 import { FindAllBoardsService } from './services/findAll.service';
 import { FindOneBoardService } from './services/findOne.service';
 import { UpdateBoardService } from './services/update.service';
@@ -33,12 +33,12 @@ import { UpdateBoardService } from './services/update.service';
     UpdateBoardService,
     FindAllBoardsService,
     FindOneBoardService,
-    DeleteNoteService,
+    DeleteBoardService,
     CreateBoardDb,
     UpdateBoardDb,
     FindAllBoardsDb,
     FindOneBoardDb,
-    DeleteNoteDb,
+    DeleteBoardDb,
   ],
   exports: [],
 })
